cce7e53a2ae6e15d10a771afdf4c10e7f8775f8c
Nicholas Njihia <njihianicklas55@gmail.com>
2020-06-26 16:36:46 +0300

    Made more edits to scripts

--- scripts/get_requisite_software.sh
+++ scripts/get_requisite_software.sh
@@ -4,15 +4,7 @@
 #--------------
 #--------------
 
-#Terraform -> enables us to specify which 
-#resources we need and which cloud provider and then proceed to deploy said resources.
-
-#Packer -> enables us to build our AMI images for the 
-#AWS EC2 resources. These are the AMI images Terraform will use.
-#Our configuration management is done via Shell or Ansible 
-#but that is handled within Packer via the Packer's provisioner block.
-
-#1. DOWNLOADING OUR SOFTWARE
+#1. DOWNLOADING OUR SOFTWARE(Packer and Terraform).
 #----------------------------
 #----------------------------
 
@@ -20,7 +12,7 @@
 #Urls and versions for Packer and Terraform may change so check these urls:
 #https://www.packer.io/downloads.html --> for Packer
 #https://learn.hashicorp.com/terraform/getting-started/install
-VERSION_NUM=1.6.0
+export VERSION_NUM="1.6.0"
 wget https://releases.hashicorp.com/packer/${VERSION_NUM}/packer_${VERSION_NUM}_linux_amd64.zip
 wget https://releases.hashicorp.com/terraform/${VERSION_NUM}/terraform_${VERSION_NUM}_linux_amd64.zip
 
@@ -67,25 +59,42 @@ function get_os_version(){
 install_for_ubuntu() {
 	sudo apt update
 	sudo apt install unzip
+	sudo apt install python3 python3-pip python3-setuptools
+	pip3 install --user ansible
 }
 install_for_red_hat() {
+	sudo yum update -y
+	sudo yum install -y epel-release 
 	sudo yum install -y unzip
+	sudo yum install -y python3 python3-pip 
+	sudo yum install ansible
 }
 install_for_fedora() {
 	sudo dnf install unzip
+	sudo dnf install python3 python3-pip 
+	sudo dnf install ansible
 }
 install_for_archlinux() {
-	sudo pacman -Syy
-	sudo pacman -S unzip
+	sudo pacman -Syyy
+	sudo pacman -Sy unzip
+	sudo pacman -Sy python3 python3-pip 
+	sudo pacman -Sy ansible
 }
 install_for_suse(){
-	sudo zypper install unzip
+	sudo zypper install -y unzip
+	sudo zypper install -y python3python3-pip 
+	pip3 install --user ansible
 }
 
 #The command below unzips the Terraform and Packer binaries and moves them to /usr/local/bin
 unzip_terraform_and_packer_binaries() {
 	unzip terraform_${VERSION_NUM}_linux_amd64.zip
 	unzip packer_${VERSION_NUM}_linux_amd64.zip
+	sudo chmod +x terraform
+	sudo chmod +x packer
+	#Move the binaries to directory that is listed as $PATH environment variable
+	sudo mv terraform packer /usr/local/bin 
+	echo "Succesfully Installed Terraform and Packer"
 }
 
 
