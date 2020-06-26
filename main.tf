1b0c5cc4583a9cc7d18358cf546f4c07bb3ed0c4
Nicholas Njihia <njihianicklas55@gmail.com>
2020-06-26 16:52:45 +0300

    Removed Cluttered files

--- main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-   profile = "default"
-   region  = "us-east-1"
-}
-resource "aws_instance" "base" {
-   ami           = ""
-   instance_type = "t2.micro"
-}
-
--- packer_generate_ami.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#Packer will give us a custom AMI that will be referenced for Terraform's provision.
-AMI_ID=`packer build -machine-readable ./packer/packer_template.json | awk -F, '$0 ~/artifact,0,id/ {print $6}'`
-echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > amivar.tf 
